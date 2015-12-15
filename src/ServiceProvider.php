a438af6e59b1db5a00b839d43b10cbc20b242803
Ben Rowe <ben.rowe.83@gmail.com>
2015-12-15 13:34:30 +1100

    Update docblocks in ServiceProvider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the laravel url package.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+
 namespace Benrowe\Laravel\Url;
 
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
@@ -8,6 +16,10 @@ use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
  * Url Service Provider
  * Registers the service provider into the application IOC
  *
+ * @package    Benrowe\Laravel\Url
+ * @author     Ben Rowe <ben.rowe.83@gmail.com>
+ * @copyright  Ben Rowe <ben.rowe.83@gmail.com>
+ * @link       https://github.com/benrowe/laravel-filesystem-url
  */
 class ServiceProvider extends LaravelServiceProvider
 {
