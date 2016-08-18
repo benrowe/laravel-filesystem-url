1d4f1e2e6c094b6973d97640595e958c41962ee9
Ben Rowe <ben.rowe.83@gmail.com>
2016-08-19 08:45:59 +1000

    Update package serviceprovider to support laravel 5.2

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * This file is part of the laravel url package.
- *
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- */
-
-
 namespace Benrowe\Laravel\Url;
 
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
@@ -18,9 +10,6 @@ use Blade;
  * Registers the service provider into the application IOC
  *
  * @package    Benrowe\Laravel\Url
- * @author     Ben Rowe <ben.rowe.83@gmail.com>
- * @copyright  Ben Rowe <ben.rowe.83@gmail.com>
- * @link       https://github.com/benrowe/laravel-filesystem-url
  */
 class ServiceProvider extends LaravelServiceProvider
 {
@@ -44,7 +33,7 @@ class ServiceProvider extends LaravelServiceProvider
 
     public function register()
     {
-        $this->app->bindShared('filesystem-url', function ($app) {
+        $this->app->singleton('filesystem-url', function ($app) {
             return new UrlService(config('filesystems'), $app['request']->secure());
         });
         $this->app->alias('filesystem-url', 'Benrowe\Laravel\Url\UrlService');
